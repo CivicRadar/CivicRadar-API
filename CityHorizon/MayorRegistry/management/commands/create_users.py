bda860745e3bbdf94e2fcd2b4acd47140aa8f38c
Arsalan-Zanganeh <a_doost@comp.iust.ac.ir>
2025-04-05 23:40:12 +0330

    Sprint 3 citizen report serializer changed

--- CityHorizon/MayorRegistry/management/commands/create_users.py
+++ CityHorizon/MayorRegistry/management/commands/create_users.py
@@ -1,5 +1,8 @@
+from django.core.files import File
 from django.core.management.base import BaseCommand
 from Authentication.models import User
+import os
+from django.conf import settings
 
 class Command(BaseCommand):
     help = 'Create users'
@@ -7,10 +10,14 @@ class Command(BaseCommand):
         user = User(FullName="علی مرادی", Email="citizen@gmail.com", Type='Citizen', Verified=True)
         user.set_password('citizen')
         user.save()
-        user = User(FullName="مجید احمدی", Email="mayor@gmail.com", Type='Mayor')
-        user.set_password('mayor')
-        user.save()
-        user = User(FullName="محسن معین‌فر", Email="acc.of.soccer@gmail.com", Type='Admin')
+        image_path = os.path.join(settings.MEDIA_ROOT, '2.jpg')
+        with open(image_path, 'rb') as img_file:
+            django_file = File(img_file)
+            user = User(FullName="مجید احمدی", Email="mayor@gmail.com", Type='Mayor')
+            user.set_password('mayor')
+            user.Picture.save('2.jpg', django_file, save=False)
+            user.save()
+        user =  User(FullName="محسن معین‌فر", Email="acc.of.soccer@gmail.com", Type='Admin')
         user.set_password('admin')
         user.save()
         user = User(FullName="رضا محمدی", Email="reza.m1746@gmail.com", Type='Admin')
