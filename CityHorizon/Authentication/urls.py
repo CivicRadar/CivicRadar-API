faf54a9a78c5b06e06d3a6213ad432014d99335c
Arrash Tahery <107276674+Arrash03@users.noreply.github.com>
2025-04-10 02:56:12 +0330

    Sprint 3 & Add Account management (#42)
    * Added theme to models and send it after login
    
    * Set-theme endpoint worked successfully

--- CityHorizon/Authentication/urls.py
+++ CityHorizon/Authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import EmailVerification, SignUp, Login, Profile, PasswordTokenCheck, RequestPasswordReset, SetNewPassword, \
-                   Logout
+                   Logout, SetTheme
 
 urlpatterns = [
     path('signup/', SignUp.as_view()),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('request-reset-email/', RequestPasswordReset.as_view(), name='request-reset-email'),
     path('password-reset/<ui64>/<token>/', PasswordTokenCheck.as_view(), name='password-reset-confirmed'),
     path('password-reset-complete/', SetNewPassword.as_view(), name='password-reset-complete'),
-    path('email-verification/<token>/', EmailVerification.as_view(), name='sign-up-email-verification')
+    path('email-verification/<token>/', EmailVerification.as_view(), name='sign-up-email-verification'),
+    path('set-theme/', SetTheme.as_view(), name='set-theme')
 ]
