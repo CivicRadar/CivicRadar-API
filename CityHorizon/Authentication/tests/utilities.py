5b4b46f008c7f0d09783bf2544928d1128c0edbd
Arrash Tahery <atv1382@gmail.com>
2025-04-11 20:59:05 +0330

    test_missing_required_fields

---
+++ CityHorizon/Authentication/tests/utilities.py
@@ -0,0 +1,7 @@
+from Authentication.models import User
+
+
+def create_user(Email, Password, FullName, Type, **extra_args):
+    user = User(FullName=FullName, Email=Email, Type=Type, **extra_args)
+    user.set_password(Password)
+    user.save()
