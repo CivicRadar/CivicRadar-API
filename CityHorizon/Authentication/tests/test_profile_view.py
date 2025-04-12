76953aa3a5cdde8e1aab910b698004a740bcc613
Arrash Tahery <atv1382@gmail.com>
2025-04-12 07:23:45 +0330

    Add test_get_without_token

--- CityHorizon/Authentication/tests/test_profile_view.py
+++ CityHorizon/Authentication/tests/test_profile_view.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, tag
 from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework.test import APIClient
+from rest_framework import status
 from django.conf import settings
 import jwt
 import datetime
@@ -108,3 +109,46 @@ class ProfileViewTests(TestCase):
         response = self.client.post(self.url, {'FullName': 'New Name'})
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json()['error'], 'User not found!')
+
+    def test_get_without_token(self):
+        """
+        If no token is present, the view should raise an AuthenticationFailed exception,
+        which typically returns a 401 response.
+        """
+        response = self.client.get(self.url)
+        # Depending on your exception handler, DRF converts the exception to a 401.
+        self.assertEqual(response.status_code, 403)
+
+    # def test_get_with_invalid_token(self):
+    #     """
+    #     Passing an invalid token should result in a 401 Unauthorized.
+    #     """
+    #     self.client.cookies.load({"jwt": "invalidtoken"})
+    #     response = self.client.get(self.url)
+    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # def test_get_with_expired_token(self):
+    #     """
+    #     An expired token should also result in a 401 Unauthorized response.
+    #     """
+    #     self.client.cookies.load({"jwt": self.expired_token})
+    #     response = self.client.get(self.url)
+    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # def test_get_with_valid_token(self):
+    #     """
+    #     A valid token should allow access.
+    #     The view should set the user's Picture field to None, save the user,
+    #     and return the serialized profile data.
+    #     """
+    #     self.client.cookies.load({"jwt": self.valid_token})
+    #     response = self.client.get(self.url)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    #     # Reload user from database after modification.
+    #     self.user.refresh_from_db()
+    #     self.assertIsNone(self.user.Picture)
+
+    #     # Check if the response contains serialized data from the ProfileSerializer.
+    #     # For example, if ProfileSerializer returns the username, we verify that.
+    #     self.assertEqual(response.data.get("username"), self.user.username)
