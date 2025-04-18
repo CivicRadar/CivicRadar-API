ab76aa6bbf5dcc9d5e585f94c50af1a28daf430b
Arsalan-Zanganeh <a_doost@comp.iust.ac.ir>
2025-04-18 05:44:12 +0330

    Sprint 4 serializer conflict resolved

--- CityHorizon/Authentication/tests/utilities.py
+++
@@ -1,8 +0,0 @@
-from Authentication.models import User
-
-
-def create_user(Email, Password, FullName, Type, **extra_args):
-    user = User(FullName=FullName, Email=Email, Type=Type, **extra_args)
-    user.set_password(Password)
-    user.save()
-    return user
