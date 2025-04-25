34a55a4a579f8e45cc75b2f908604ec558343063
Arsalan-Zanganeh <a_doost@comp.iust.ac.ir>
2025-04-26 03:05:02 +0330

    Sprint 5 single province count problems count added

--- CityHorizon/IssueSupervision/urls.py
+++ CityHorizon/IssueSupervision/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import CitizenReportProblem, CitizenReportCitizen, AllCitizenReport, MayorCityReports, \
                    MayorNotes, MayorDetermineCityProblemSituation, MayorPrioritize, MayorDelegate, MayorDedicatedReportPage, \
-                   PublicReport, ReportCount, CityReportCount, CitiesReportCount, ProvincesReportCount
+                   PublicReport, ReportCount, CityReportCount, CitiesReportCount, ProvincesReportCount, ProvinceReportCount
 
 urlpatterns = [
     path('citizen-report-problem/', CitizenReportProblem.as_view()),
@@ -18,4 +18,5 @@ urlpatterns = [
     path('city-report-count/', CityReportCount.as_view()),
     path('cities-report-count/', CitiesReportCount.as_view()),
     path('provinces-report-count/', ProvincesReportCount.as_view()),
+    path('province-report-count/', ProvinceReportCount.as_view()),
 ]
