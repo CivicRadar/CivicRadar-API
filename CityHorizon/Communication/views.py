1b3564fc46570d27bad1adcada5dc9e7d7a35037
Arsalan-Zanganeh <a_doost@comp.iust.ac.ir>
2025-04-26 04:55:11 +0330

    Sprint 5 normal chat and reactions implemented

--- CityHorizon/Communication/views.py
+++ CityHorizon/Communication/views.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from Authentication.models import User, Notification, CityProblemReaction, CityProblem
-from .serializers import NotoficationSerializer, CityProblemReactionSerializer, PointsSerializer
+from Authentication.models import User, Notification, CityProblemReaction, CityProblem, Comment, CommentReaction
+from .serializers import NotoficationSerializer, CityProblemReactionSerializer, PointsSerializer, CommentSerializer, CommentReactionSerializer
 import jwt, datetime
 
 
@@ -122,3 +122,119 @@ class Points(APIView):
             raise AuthenticationFailed("User not found!")
         serializer =  PointsSerializer(user, context={'request':request})
         return Response(serializer.data)
+
+class Comments(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed("Unauthenticated!")
+
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Expired token!")
+
+        user = User.objects.filter(id=payload['id']).first()
+        if user is None:
+            raise AuthenticationFailed("User not found!")
+        if user.Type == 'Admin':
+            raise AuthenticationFailed("You can't comment as an admin!")
+        myvar = request.query_params.get('CityProblemID')
+        if myvar is None:
+            raise AuthenticationFailed("variable name is wrong or value is null")
+        cprobe = CityProblem.objects.filter(id=myvar).first()
+        if cprobe is None:
+            raise AuthenticationFailed("City problem not found!")
+        comments = Comment.objects.filter(CityProblem=cprobe).all()
+        serialzier = CommentSerializer(comments, many=True)
+        return Response(serialzier.data)
+
+    def post(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed("Unauthenticated!")
+
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Expired token!")
+
+        user = User.objects.filter(id=payload['id']).first()
+        if user is None:
+            raise AuthenticationFailed("User not found!")
+        if user.Type == 'Admin':
+            raise AuthenticationFailed("You can't comment as an admin!")
+        cprobe = CityProblem.objects.filter(id=request.data['CityProblemID']).first()
+        if cprobe is None:
+            raise AuthenticationFailed("City problem not found!")
+        comment = Comment(Sender=user, Content=request.data['Content'],
+                          CityProblem=cprobe,
+                          IsAReply=request.data['IsAReply'],
+                          ReplyID=request.data['ReplyID'])
+        comment.full_clean()
+        comment.save()
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+class CommentReactions(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed("Unauthenticated!")
+
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Expired token!")
+
+        user = User.objects.filter(id=payload['id']).first()
+        if user is None:
+            raise AuthenticationFailed("User not found!")
+        if user.Type == 'Admin':
+            raise AuthenticationFailed("You can't comment as an admin!")
+
+        comment = Comment.objects.filter(id=request.query_params['CommentID']).first()
+        if comment is None:
+            raise AuthenticationFailed("City problem not found!")
+        react = CommentReaction.objects.filter(Reactor=user, Comment=comment).first()
+        if react is None:
+            return Response({"Like":None})
+        return Response({"Like":react.Like})
+
+    def post(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed("Unauthenticated!")
+
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Expired token!")
+
+        user = User.objects.filter(id=payload['id']).first()
+        if user is None:
+            raise AuthenticationFailed("User not found!")
+        if user.Type == 'Admin':
+            raise AuthenticationFailed("You can't comment as an admin!")
+
+        comment = Comment.objects.filter(id=request.data['CommentID']).first()
+        if comment is None:
+            raise AuthenticationFailed("Comment not found!")
+        react = CommentReaction.objects.filter(Reactor=user, Comment=comment).first()
+        if react is None:
+            newreact = CommentReaction(Reactor=user, Comment=comment, Like=request.data['Like'])
+            newreact.save()
+            serializer = CommentReactionSerializer(newreact)
+            return Response(serializer.data)
+        elif react.Like == request.data['Like']:
+            react.delete()
+            return Response({"Like":None})
+        else:
+            react.Like = request.data['Like']
+            react.save()
+            serializer = CommentReactionSerializer(react)
+            return Response(serializer.data)
